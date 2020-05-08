e758bad98eb2433dc85b664d30e2c80d51f5cf06
Dmitriy Panfilov <divasik@gmail.com>
2020-05-08 11:55:21 +0300

    all tasks

---
+++ lib/models/user.dart
@@ -0,0 +1,120 @@
+import '../string_utils.dart';
+
+enum LoginType { email, phone }
+
+class User {
+  String email;
+  String phone;
+
+  String _lastName;
+  String _firstName;
+
+  LoginType _type;
+
+  List<User> friends = [];
+
+  User._({String firstName, String lastName, String phone, String email})
+      : _firstName = firstName,
+        _lastName = lastName,
+        this.phone = phone,
+        this.email = email {
+    print("User is created");
+    _type = email != null ? LoginType.email : LoginType.phone;
+  }
+
+  factory User({String name, String phone, String email}) {
+    if (name.isEmpty) throw Exception("User name is empty");
+    if (phone != null && phone.isEmpty || email != null && email.isEmpty) throw Exception(
+        "phone or email is empty"
+    );
+
+    return User._(
+        firstName: _getFirstName(name),
+        lastName: _getLastName(name),
+        phone: phone != null ? checkPhone(phone) : null,
+        email: email != null ? checkEmail(email) : null
+    );
+  }
+
+  String get login {
+    return _type == LoginType.phone ? phone : email;
+  }
+
+  String get name {
+    return "${_firstName.capitalize()} ${_lastName.capitalize()}";
+  }
+
+
+  @override
+  bool operator ==(other) {
+    if (other == null) {
+      return false;
+    }
+
+    if (other is User) {
+      return _firstName == other._firstName && _lastName == other._lastName &&
+          (phone == other.phone || email == other.email);
+    }
+  }
+
+  void addFriends(Iterable<User> newFriends) {
+    friends.addAll(newFriends);
+  }
+
+  void removeFriend(User friend) {
+    friends.remove(friend);
+  }
+
+  String get userInfo => '''
+    name: $name
+    email: $email
+    firstName: $_firstName
+    lastName: $_lastName
+    friends: ${friends.toList()}
+  ''';
+
+
+  @override
+  String toString() {
+    return '''
+    name: $name
+    email: $email
+    friends: ${friends.toList()}
+  ''';
+  }
+
+  static String _getLastName(String userName) => userName.split(" ")[1];
+
+  static String _getFirstName(String userName) => userName.split(" ")[0];
+
+  static String checkPhone(String phone) {
+    String pattern = r"^(?:[+])?[0-9]{11}";
+    phone = phone.replaceAll(RegExp("[^+\\d]"), "");
+
+    if (phone == null || phone.isEmpty) {
+      throw Exception("Enter don't empty phone number");
+    } else if (!RegExp(pattern).hasMatch(phone)) {
+      throw Exception(
+          "Enter a valid phone number starting with a + and containing 11 digits");
+    }
+
+    return phone;
+  }
+
+  static String checkEmail(String email) {
+    if (email == null || email.isEmpty) {
+      throw Exception("Enter not empty email");
+    }
+    if(!email.contains("@")) {
+      throw Exception("Email is not valid registerUserByPhone");
+    }
+
+    return email;
+  }
+}
+
+mixin UserUtils {
+  String capitalize(String s) {
+    return s.capitalize();
+  }
+}
---
+++ lib/user_holder.dart
@@ -0,0 +1,80 @@
+import 'models/user.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email){
+    User user = User(name: name, phone: phone, email: email);
+
+    if(!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("User with this name already exist");
+    }
+  }
+
+  User getUserByLogin(String login) {
+    return users[login];
+  }
+
+  List<User> importUsers(List<String> strUsers) {
+    List<User> users = [];
+    strUsers.forEach((str) {
+      List<String> tokens = str.split(";");
+      tokens.remove("\"\"\"");
+      User u = User(name: tokens[0], email: tokens[1], phone: tokens[2]);
+      users.add(u);
+    });
+    return users;
+  }
+
+  void setFriends(String login, List<User> friends) {
+    User user = getUserByLogin(login);
+    if(user != null) {
+      user.friends = friends;
+    }
+  }
+
+  User registerUserByEmail(String fullName, String email) {
+    User user = User(name: fullName, email: email);
+
+    if(!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception('Email is not valid registerUserByPhone');
+    }
+
+    return user;
+  }
+
+  User registerUserByPhone(String fullName, String phone) {
+    User user = User(name: fullName, phone: phone);
+
+    users.forEach((_, value) {
+      if(value.phone == user.phone) {
+        throw Exception('A user with this phone already exists');
+      }
+    });
+
+    if(!users.containsKey(user.login)) {
+      users[user.login] = user;
+    }
+
+    return user;
+  }
+
+  User findUserInFriends(String fullName, User user) {
+    User targetUser = users.values.firstWhere(
+            (u) => u.name == fullName,
+        orElse: () => null);
+
+    if(targetUser == null) {
+      throw Exception("User $fullName not registered");
+    }
+
+    User friend = targetUser.friends.firstWhere(
+            (u) => u == user,
+        orElse: () => throw Exception("${user.login} is not a friend of the login")
+    );
+  }
+}
